faead0c575458fe59c3be8ff2e029ffc294b40ff
Daniela Pech <randakova.daniela@gmail.com>
2025-05-17 10:37:08 +0200

    zprovozneni routovani

---
+++ src/components/ReservationPage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+export const ReservationPage = () => (
+  <div className="container">
+    <h2>Vaše e-jízdenka</h2>
+    <p>Tady bude seznam rezervací.</p>
+  </div>
+);
