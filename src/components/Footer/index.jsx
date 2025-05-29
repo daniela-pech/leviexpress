16da1576eca90586661d6756992618cbbd733658
Daniela Pech <randakova.daniela@gmail.com>
2025-05-29 23:04:38 +0200

    automaticky vyber sedadla

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
 import './style.css';
 
 export const Footer = () => (
   <footer>
     <div className="container">
-      <div className="footer-title">
-        LeviExpress
-      </div>
+      <div className="footer-title">LeviExpress</div>
       <div className="copyright">
         Czechitas Digitální akademie: Web, © 2021 - 2023
       </div>
